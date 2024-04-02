1dabd4cff9f7cc9ae1b477124fba71c7f2920d20
enekomtz1 <enekomartinezm@gmail.com>
2024-04-02 15:06:59 +0200

    Updating code

--- 01-ChatApp/Back-End/Controllers/message.controler.js
+++ 01-ChatApp/Back-End/Controllers/message.controler.js
@@ -1,8 +1,16 @@
+import Conversation from "../Models/conversation.model.js";
+
 export const sendMessage = async (req, res) => {
 	try {
 		const { message } = req.body;
-		const { id } = req.params;
-		const senderId = req.userId;
+		const { id: receiverId } = req.params;
+		const senderId = req.user._id;
+
+		let conversation = await Conversation.findOne({
+			participants: { $all: [senderId, receiverId] },
+		});
+
+		
 	} catch (error) {
 		res.status(500).json({ error: "Internal server error" });
 	}
