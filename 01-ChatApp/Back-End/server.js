12f08760bc31f3d7dd1c9b44ee8484eb61a9d63d
enekomtz1 <enekomartinezm@gmail.com>
2024-04-03 19:06:07 +0200

    Get users for sidebar endpoint

--- 01-ChatApp/Back-End/server.js
+++ 01-ChatApp/Back-End/server.js
@@ -1,10 +1,14 @@
+// Package imports:
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+// Routes:
 import authRoutes from "./Routes/auth.routes.js";
 import messageRoutes from "./Routes/message.routes.js";
+import userRoutes from "./Routes/user.routes.js";
 
+// Data Base imports:
 import connectToMongoDB from "./DataBase/connectToMongoDB.js";
 
 dotenv.config();
@@ -17,6 +21,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 /*
 app.get("/", (req, res) => {
 	// root route http://localhost:5000/
