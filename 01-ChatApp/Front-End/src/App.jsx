1f491629b4c369553ff433e48311f522c792503d
enekomtz1 <enekomartinezm@gmail.com>
2024-04-04 18:54:14 +0200

    Adding changes to the Messages display

--- 01-ChatApp/Front-End/src/App.jsx
+++ 01-ChatApp/Front-End/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/SignUp";
+import Home from "./Pages/Home/Home";
 
 function App() {
 	return (
 		<div className="p-4 h-screen flex items-center justify-center">
-			<Signup></Signup>
+			{/*<Signup></Signup>*/}
+			<Home />
 		</div>
 	);
 }
