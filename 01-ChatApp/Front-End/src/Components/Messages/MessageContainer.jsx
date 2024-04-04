b91677b169cc6706b218554accc1ca435d192182
enekomtz1 <enekomartinezm@gmail.com>
2024-04-04 19:10:21 +0200

    Adding message sender

--- 01-ChatApp/Front-End/src/Components/Messages/MessageContainer.jsx
+++ 01-ChatApp/Front-End/src/Components/Messages/MessageContainer.jsx
@@ -1,3 +1,4 @@
+import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 
 const MessageContainer = () => {
@@ -6,11 +7,36 @@ const MessageContainer = () => {
 			<>
 				<div className="bg-slate-500 px-4 py-2 mb-2">
 					<span className="label-text">To: </span> <span className="text-gray-900 font-bold">John Doe</span>
-					<Messages />
 				</div>
+				<Messages />
+				<MessageInput />
 			</>
 		</div>
 	);
 };
 
 export default MessageContainer;
+
+/*
+STARTER CODE SNIPPET
+
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+
+const MessageContainer = () => {
+	return (
+		<div className="md:min-w-[450px] flex flex-col">
+			<>
+				<div className="bg-slate-500 px-4 py-2 mb-2">
+					<span className="label-text">To: </span> <span className="text-gray-900 font-bold">John Doe</span>
+				</div>
+				<Messages />
+				<MessageInput />
+			</>
+		</div>
+	);
+};
+
+export default MessageContainer;
+
+*/
