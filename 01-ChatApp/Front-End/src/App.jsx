bf4dbc2e0ab6b17d40e76bfc75982ba403d00f7e
enekomtz1 <enekomartinezm@gmail.com>
2024-04-04 14:07:17 +0200

    Updating Sign Up

--- 01-ChatApp/Front-End/src/App.jsx
+++ 01-ChatApp/Front-End/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/SignUp";
 
 function App() {
 	return (
 		<div className="p-4 h-screen flex items-center justify-center">
-			<Login></Login>
+			<Signup></Signup>
 		</div>
 	);
 }
