9e11512d04432a3b51e6f2531cd0ce043e88575a
enekomtz1 <enekomartinezm@gmail.com>
2024-04-04 12:42:30 +0200

    Adding Front-End code

--- 01-ChatApp/Front-End/src/App.jsx
+++ 01-ChatApp/Front-End/src/App.jsx
@@ -2,15 +2,9 @@ import "./App.css";
 
 function App() {
 	return (
-		<>
-			<button className="btn">Button</button>
-			<button className="btn btn-neutral">Neutral</button>
-			<button className="btn btn-primary">Primary</button>
-			<button className="btn btn-secondary">Secondary</button>
-			<button className="btn btn-accent">Accent</button>
-			<button className="btn btn-ghost">Ghost</button>
-			<button className="btn btn-link">Link</button>
-		</>
+		<div className="p-4 h-screen flex items-center justify-center">
+			<Login></Login>
+		</div>
 	);
 }
 
