73e94d14717d4128f55d388be52e9077db63c738
enekomtz1 <enekomartinezm@gmail.com>
2024-04-05 10:35:26 +0200

    Adding changes

--- 01-ChatApp/Front-End/src/App.jsx
+++ 01-ChatApp/Front-End/src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/SignUp";
 import Home from "./Pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="p-4 h-screen flex items-center justify-center">
-			{/*<Signup></Signup>*/}
-			<Home />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+			</Routes>
 		</div>
 	);
 }
