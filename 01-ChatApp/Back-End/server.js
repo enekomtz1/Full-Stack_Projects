6b380e5aa678b6f970b5ec4797995e7ccf7b1e2e
enekomtz1 <enekomartinezm@gmail.com>
2024-04-08 12:33:17 +0200

    Updating server.js

--- 01-ChatApp/Back-End/server.js
+++ 01-ChatApp/Back-End/server.js
@@ -1,35 +1,38 @@
+// Package imports:
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
-import userRoutes from "./routes/user.routes.js";
+// Routes:
+import authRoutes from "./Routes/auth.routes.js";
+import messageRoutes from "./Routes/message.routes.js";
+import userRoutes from "./Routes/user.routes.js";
 
-import connectToMongoDB from "./db/connectToMongoDB.js";
-import { app, server } from "./socket/socket.js";
-
-const PORT = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
+// Data Base imports:
+import connectToMongoDB from "./DataBase/connectToMongoDB.js";
+import { app, server } from "./Socket/socket.js";
 
 dotenv.config();
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json()); // To parse incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/Front-End/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "Front-End", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
 	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
