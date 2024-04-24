d702100cf6b17f35f74aea12e65aa566bbea15c7
enekomtz1 <enekomartinezm@gmail.com>
2024-04-24 11:04:39 +0200

    Adding followUnfollowUser

--- 03-SocialMediaApp/backend/controllers/userController.js
+++ 03-SocialMediaApp/backend/controllers/userController.js
@@ -112,3 +112,35 @@ const logoutUser = (req, res) => {
 		console.log("Error in signupUser: ", err.message);
 	}
 };
+
+const followUnfollowUser = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const userToModify = await User.findById(id);
+		const currentUser = await User.findById(req.user._id);
+
+		if (id === currentUser.toString()) {
+			return res.status(400).json({ error: "You can't follow/unfollow yourserlf." });
+		}
+
+		const isFollowing = currentUser.following.includes(id);
+
+		// Unfollow if the user is already following:
+		if (isFollowing) {
+			await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+			await User.findByIdAndUpdate(req.user._id, { $pull: { followers: id } });
+
+			res.status(200).json({ message: "User unfollowed successfully" });
+		}
+		// Follow if the user is not following:
+		else {
+			await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+			await User.findByIdAndUpdate(req.user._id, { $push: { followers: id } });
+
+			res.status(200).json({ message: "User followed successfully" });
+		}
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		console.log("Error in followUnFollowUser: ", err.message);
+	}
+};
