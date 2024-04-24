696abf2caf294c83970757e09526e3b51d3d512a
enekomtz1 <enekomartinezm@gmail.com>
2024-04-24 11:29:47 +0200

    Adding updateUser

--- 03-SocialMediaApp/backend/controllers/userController.js
+++ 03-SocialMediaApp/backend/controllers/userController.js
@@ -144,3 +144,64 @@ const followUnfollowUser = async (req, res) => {
 		console.log("Error in followUnFollowUser: ", err.message);
 	}
 };
+
+const updateUser = async (req, res) => {
+	const { name, email, username, password, bio } = req.body;
+	let { profilePic } = req.body;
+
+	const userId = req.user._id;
+
+	try {
+		let user = await User.findById(userId);
+		if (!user) {
+			return res.status(400).json({ error: "User not found" });
+		}
+
+		if (req.params.id !== userId.toString()) {
+			return res.status(400).json({ error: "You can't update other user's profile" });
+		}
+
+		if (password) {
+			const salt = await bcrypt.genSalt(10);
+			const hashedPassword = await bcrypt.hash(password, salt);
+			user.password = hashedPassword;
+		}
+
+		// Update the new pic on cloudinary if profilePic exists:
+		if (profilePic) {
+			await cloudinary.uploader.destroy(user.profilePic.split("/").pop().split(".")[0]);
+		} else {
+			const uploadedResponse = await cloudinary.uploader.upload(profilePic);
+			profilePic = uploadedResponse.secure_url;
+		}
+
+		user.name = name || user.name;
+		user.email = email || user.email;
+		user.username = username || user.username;
+		user.profilePic = profilePic || user.profilePic;
+		user.bio = bio || user.bio;
+
+		// Update all changes that the user has done to the profile:
+		user = await user.save();
+
+		// Find all posts that this user replied and update username and userProfilePic fields:
+		await Post.updateMany(
+			{ "replies.userId": userId },
+			{
+				$set: {
+					"replies.$[reply].username": user.username,
+					"replies.$[reply].userProfilePic": user.profilePic,
+				},
+			},
+			{ arrayFilters: [{ "reply.userId": userId }] }
+		);
+
+		// password should be null in response
+		user.password = null;
+
+		res.status(200).json(user);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+		console.log("Error in updateUser: ", err.message);
+	}
+};
