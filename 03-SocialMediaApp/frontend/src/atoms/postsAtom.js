2c4fd3eba7b0b0b5e124dcd71c056db2559af06f
enekomtz1 <enekomartinezm@gmail.com>
2024-05-09 13:17:30 +0200

    Commenting frontend/src/atoms /postsAtom.js

--- 03-SocialMediaApp/frontend/src/atoms/postsAtom.js
+++ 03-SocialMediaApp/frontend/src/atoms/postsAtom.js
@@ -1,19 +1,19 @@
 /*
-- This code defines a state management atom using the Recoil library.
-- The atom is intended to manage the state of posts within an application.
-- It is named 'postsAtom' and serves as a unique identifier for the atom.
-- Initially, the default state of this atom is set to an empty array.
-- The atom can be imported and used in different parts of the application to maintain consistent state.
+- This code initializes a Recoil atom.
+- It is used to manage the state of posts in a React application.
+- The atom is named 'postsAtom' and it will be identifiable by this key across the application.
+- It sets an initial default state as an empty array.
+- This state can be shared and updated across different components using Recoil hooks.
 */
 
-// Import the 'atom' function from the 'recoil' package
+// Importing the 'atom' function from the 'recoil' package to create atom.
 import { atom } from "recoil";
 
-// Define 'postsAtom' using the atom function
+// Creating 'postsAtom' to hold the state of posts.
 const postsAtom = atom({
-	key: "postsAtom", // Unique key for the atom, used for identifying the atom in the application
-	default: [], // Default value for the atom's state, initially an empty array
+	key: "postsAtom", // Unique key for referring to the atom
+	default: [], // Initial state is set to an empty array
 });
 
-// Export the 'postsAtom' to be used in other parts of the application
+// Exporting the 'postsAtom' for use in other parts of the application.
 export default postsAtom;
