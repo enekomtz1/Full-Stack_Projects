a84d5e09d71bb80a66b26a2f489708e65a7a9a1e
enekomtz1 <enekomartinezm@gmail.com>
2024-05-10 18:08:45 +0200

    Commenting backend/models /user.model.js

---
+++ 04-ExpenseTrackerApp/backend/models/user.model.js
@@ -0,0 +1,7 @@
+/*
+- This code defines a Mongoose schema for a User model in a MongoDB database.
+- It specifies various fields like username, name, password, profilePicture, and gender.
+- Each field is defined with specific data types and constraints, such as 'required' and 'unique'.
+- The 'profilePicture' field has a default value of an empty string if not provided.
+- Timestamps are automatically added to each document, recording creation and update times.
+*/
