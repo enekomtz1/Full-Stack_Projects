0fcb03210442ae8d113380397ffeb5fcbe5a9cf5
enekomtz1 <enekomartinezm@gmail.com>
2024-05-10 18:09:20 +0200

    commenting frontend/src/components /Actions.jsx

--- 04-ExpenseTrackerApp/backend/models/user.model.js
+++ 04-ExpenseTrackerApp/backend/models/user.model.js
@@ -5,3 +5,5 @@
 - The 'profilePicture' field has a default value of an empty string if not provided.
 - Timestamps are automatically added to each document, recording creation and update times.
 */
+
+import mongoose from "mongoose"; // Import Mongoose to interact with MongoDB.
