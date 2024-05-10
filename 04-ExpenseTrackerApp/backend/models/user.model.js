8e66551bba7da3f1c9c8ea76b8f4b825a0e1f375
enekomtz1 <enekomartinezm@gmail.com>
2024-05-10 18:10:23 +0200

    Commenting frontend/src/components /Actions.jsx

--- 04-ExpenseTrackerApp/backend/models/user.model.js
+++ 04-ExpenseTrackerApp/backend/models/user.model.js
@@ -9,3 +9,34 @@
 import mongoose from "mongoose"; // Import Mongoose to interact with MongoDB.
 
 // Define the schema for the User model with specific attributes and constraints.
+const userSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String, // Data type of the username is string.
+			required: true, // This field is mandatory.
+			unique: true, // Username must be unique across all User documents.
+		},
+		name: {
+			type: String, // Data type for the name field.
+			required: true, // This field is also mandatory.
+		},
+		password: {
+			type: String, // Data type for the password.
+			required: true, // This field is required for security reasons.
+		},
+		profilePicture: {
+			type: String, // Data type for the profile picture.
+			default: "", // Default value for the profile picture is an empty string.
+		},
+		gender: {
+			type: String, // Data type for gender.
+			enum: ["male", "female"], // Restricts the value to 'male' or 'female'.
+		},
+	},
+	{ timestamps: true } // Automatically manage createdAt and updatedAt timestamps.
+);
+
+// Create a model based on the schema.
+const User = mongoose.model("User", userSchema);
+
+export default User; // Export the User model for use elsewhere in the project.
