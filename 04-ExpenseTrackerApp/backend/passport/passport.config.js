9ffa002df1cfee8a88ce86b6449ee3ed5412943e
enekomtz1 <enekomartinezm@gmail.com>
2024-05-11 19:46:49 +0200

    Starting backend/passport /passport.config.js

---
+++ 04-ExpenseTrackerApp/backend/passport/passport.config.js
@@ -0,0 +1,7 @@
+/*
+- This code configures Passport.js authentication strategies for a GraphQL server.
+- It includes serialization and deserialization methods to manage user sessions.
+- A custom local strategy is used to authenticate users based on username and password.
+- Error handling is integrated to manage incorrect username or password inputs securely.
+- Bcrypt.js is utilized to safely compare hashed passwords.
+*/
