46258cfae5c2278cca7883caa7571e8319f1bd51
enekomtz1 <enekomartinezm@gmail.com>
2024-05-12 14:46:49 +0200

    Adding code to index.js

---
+++ 04-ExpenseTrackerApp/backend/resolvers/index.js
@@ -0,0 +1,7 @@
+/*
+- This code integrates GraphQL resolver modules for a server application.
+- It uses the mergeResolvers function from the @graphql-tools/merge package.
+- The code imports resolver modules specifically for user and transaction data.
+- It merges these two resolver modules into a single resolver object.
+- The merged resolvers are then exported for use in GraphQL server setup.
+*/
