ef0322cdd2eb2704a72850507731983b52884427
enekomtz1 <enekomartinezm@gmail.com>
2024-05-13 11:28:38 +0200

    Adding code

---
+++ 04-ExpenseTrackerApp/backend/resolvers/transaction.resolver.js
@@ -0,0 +1,7 @@
+/*
+- This code defines a GraphQL resolver named transactionResolver for handling transaction data.
+- It includes queries to fetch transactions, individual transactions, and category statistics.
+- Additionally, it contains mutations to create, update, and delete transactions.
+- Error handling is implemented to manage unauthorized access and other transaction-related errors.
+- The resolver utilizes Mongoose models to interact with a MongoDB database for transaction data.
+*/
