4f8caa23f3a48f94fac9e80b80873d8d7f24897b
enekomtz1 <enekomartinezm@gmail.com>
2024-05-13 11:31:57 +0200

    Adding code

--- 04-ExpenseTrackerApp/backend/resolvers/transaction.resolver.js
+++ 04-ExpenseTrackerApp/backend/resolvers/transaction.resolver.js
@@ -7,4 +7,22 @@
 */
 
 import Transaction from "../models/transaction.model.js"; // Import the Transaction model
-import User from "../models/user.model.js";  // Import the User model
+import User from "../models/user.model.js"; // Import the User model
+
+const transactionResolver = {
+	Query: {
+		// Query to fetch all transactions associated with a user
+		transactions: async (_, __, context) => {
+			try {
+				if (!context.getUser()) throw new Error("Unauthorized"); // Check if the user is authenticated
+				const userId = await context.getUser()._id; // Get the user ID from the context
+
+				const transactions = await Transaction.find({ userId }); // Fetch transactions from the database by user ID
+				return transactions; // Return the fetched transactions
+			} catch (err) {
+				console.error("Error getting transactions:", err); // Log and throw errors encountered
+				throw new Error("Error getting transactions");
+			}
+		},
+	},
+};
