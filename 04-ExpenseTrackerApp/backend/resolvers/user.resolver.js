562e439d34137ae8b047172abe21817df4c202f6
enekomtz1 <enekomartinezm@gmail.com>
2024-05-14 11:30:32 +0200

    Adding code

---
+++ 04-ExpenseTrackerApp/backend/resolvers/user.resolver.js
@@ -0,0 +1,7 @@
+/*
+- This code defines user authentication and account management operations for a web application.
+- It includes GraphQL resolvers for signing up, logging in, and logging out users.
+- The signUp resolver handles user registration, including password hashing and profile picture assignment.
+- The login and logout resolvers manage user sessions.
+- Additionally, the code provides queries to retrieve the authenticated user or a specific user by ID.
+*/
