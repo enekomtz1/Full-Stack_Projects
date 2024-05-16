c0fff5a96a5c6b7fd5f3fe28422b888a36a3c292
enekomtz1 <enekomartinezm@gmail.com>
2024-05-16 18:40:53 +0200

    Adding code

--- 03-SocialMediaApp/frontend/src/pages/ChatPage.jsx
+++ 03-SocialMediaApp/frontend/src/pages/ChatPage.jsx
@@ -5,3 +5,15 @@
 - Socket events are handled to update message status in real-time.
 - It fetches and displays conversations from an API endpoint.
 */
+
+import { SearchIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, Input, Skeleton, SkeletonCircle, Text, useColorModeValue } from "@chakra-ui/react";
+import Conversation from "../components/Conversation";
+import { GiConversation } from "react-icons/gi";
+import MessageContainer from "../components/MessageContainer";
+import { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { conversationsAtom, selectedConversationAtom } from "../atoms/messagesAtom";
+import userAtom from "../atoms/userAtom";
+import { useSocket } from "../context/SocketContext";
