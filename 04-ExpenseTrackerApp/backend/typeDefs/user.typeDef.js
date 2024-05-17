318888ac7a23e92bb9bf1aa7442d2d7d5ad607b5
enekomtz1 <enekomartinezm@gmail.com>
2024-05-17 19:18:04 +0200

    Adding code

--- 04-ExpenseTrackerApp/backend/typeDefs/user.typeDef.js
+++ 04-ExpenseTrackerApp/backend/typeDefs/user.typeDef.js
@@ -5,3 +5,5 @@
 - It includes mutations for user sign-up, login, and logout operations.
 - Input types for sign-up and login are defined for structured data input.
 */
+
+// Define the GraphQL schema for user-related operations
