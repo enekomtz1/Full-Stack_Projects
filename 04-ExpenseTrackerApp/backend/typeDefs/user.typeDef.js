ed6c3c7aa000f0257b4504c5e078b4231b4d5b2b
enekomtz1 <enekomartinezm@gmail.com>
2024-05-17 19:19:45 +0200

    Adding code

--- 04-ExpenseTrackerApp/backend/typeDefs/user.typeDef.js
+++ 04-ExpenseTrackerApp/backend/typeDefs/user.typeDef.js
@@ -7,6 +7,50 @@
 */
 
 // Define the GraphQL schema for user-related operations
-
 const userTypeDef = `#graphql
-// Define the User type with necessary fields and their types
+  // Define the User type with necessary fields and their types
+  type User {
+    _id: ID!                   // Unique identifier for the user
+    username: String!          // Username of the user
+    name: String!              // Name of the user
+    password: String!          // Password of the user
+    profilePicture: String     // Optional profile picture URL
+    gender: String!            // Gender of the user
+    transactions: [Transaction!] // List of transactions associated with the user
+  }
+
+  // Define queries for fetching user data
+  type Query {
+    authUser: User             // Fetch the authenticated user's details
+    user(userId:ID!): User     // Fetch a specific user's details by userId
+  }
+
+  // Define mutations for user sign-up, login, and logout
+  type Mutation {
+    signUp(input: SignUpInput!): User // Sign-up a new user with the provided input
+    login(input: LoginInput!): User   // Log in an existing user with the provided input
+    logout: LogoutResponse            // Log out the current user
+  }
+
+  // Define input type for user sign-up
+  input SignUpInput {
+    username: String!          // Username for the new user
+    name: String!              // Name of the new user
+    password: String!          // Password for the new user
+    gender: String!            // Gender of the new user
+  }
+
+  // Define input type for user login
+  input LoginInput {
+    username: String!          // Username for logging in
+    password: String!          // Password for logging in
+  }
+
+  // Define the response type for logout operation
+  type LogoutResponse {
+    message: String!           // Logout response message
+  }
+`;
+
+// Export the userTypeDef for use in other parts of the application
+export default userTypeDef;
