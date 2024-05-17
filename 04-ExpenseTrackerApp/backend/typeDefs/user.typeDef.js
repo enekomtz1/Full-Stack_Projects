d1346c0f3b60348d1a85c050e30dbe5f62943fb7
enekomtz1 <enekomartinezm@gmail.com>
2024-05-17 19:17:55 +0200

    Adding code

---
+++ 04-ExpenseTrackerApp/backend/typeDefs/user.typeDef.js
@@ -0,0 +1,7 @@
+/*
+- This code defines the GraphQL schema for user-related operations.
+- It specifies the User type with necessary fields and their types.
+- It includes queries to fetch authenticated user details and a specific user's details.
+- It includes mutations for user sign-up, login, and logout operations.
+- Input types for sign-up and login are defined for structured data input.
+*/
