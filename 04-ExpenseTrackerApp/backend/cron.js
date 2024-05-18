0df51698b1b8c600a7d3b8c2ac013a8b000668a9
enekomtz1 <enekomartinezm@gmail.com>
2024-05-18 19:30:32 +0200

    Adding code

--- 04-ExpenseTrackerApp/backend/cron.js
+++ 04-ExpenseTrackerApp/backend/cron.js
@@ -1,5 +1,6 @@
 /*
 - This code schedules a cron job that sends an HTTP GET request to a specified URL every 14 minutes.
 - It uses the `cron` package to define and manage the job schedule.
+- The job sends a GET request to the server and logs a success message if the response status is 200.
 
 */
