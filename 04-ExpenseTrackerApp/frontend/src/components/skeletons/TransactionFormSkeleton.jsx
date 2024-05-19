360469b7cd4ad541298f7ccacf269afa37f94350
enekomtz1 <enekomartinezm@gmail.com>
2024-05-19 19:52:46 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/components/skeletons/TransactionFormSkeleton.jsx
+++ 04-ExpenseTrackerApp/frontend/src/components/skeletons/TransactionFormSkeleton.jsx
@@ -2,5 +2,6 @@
 - This code defines a React functional component called TransactionFormSkeleton.
 - It serves as a placeholder or loading skeleton for a form used in transactions.
 - The component helps improve user experience by showing a loading animation while data is being fetched.
+- It uses Tailwind CSS classes for styling and animations.
 
 */
