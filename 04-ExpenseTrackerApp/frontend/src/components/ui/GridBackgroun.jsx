5a72e00ce83aaf5e5a164ce89fc14642fa824dc4
enekomtz1 <enekomartinezm@gmail.com>
2024-05-20 20:09:59 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/components/ui/GridBackgroun.jsx
+++ 04-ExpenseTrackerApp/frontend/src/components/ui/GridBackgroun.jsx
@@ -1,5 +1,6 @@
 /*
 - This code defines a React functional component named GridBackground.
 - It provides a styled container with a grid-like background and a radial gradient overlay.
+- The component accepts children elements and renders them within the styled container.
 
 */
