ddf50fc988e63a9ff72d4a1b486d28a9164e77f0
enekomtz1 <enekomartinezm@gmail.com>
2024-05-21 21:22:18 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/components/ui/Header.jsx
+++ 04-ExpenseTrackerApp/frontend/src/components/ui/Header.jsx
@@ -2,6 +2,7 @@
 - This code defines a React component called `Header`.
 - It renders a styled header section for a web page.
 - The header includes a title with responsive text sizes.
+- It uses React Router's `Link` for navigation within the app.
 
 
 */
