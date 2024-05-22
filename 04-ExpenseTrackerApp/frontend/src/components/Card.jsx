7b520362db0371a47c2aa4b102c8c75d65e3b80c
enekomtz1 <enekomartinezm@gmail.com>
2024-05-22 13:33:02 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/components/Card.jsx
+++ 04-ExpenseTrackerApp/frontend/src/components/Card.jsx
@@ -1,5 +1,6 @@
 /*
 - This code defines a React component named Card.
 - It displays a transaction's details such as category, amount, location, date, payment type, and description.
+- The component also provides functionality to delete a transaction.
 
 */
