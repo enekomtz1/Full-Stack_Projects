58fe13a8360b8cb4646f40d6099e1ef12e129a88
enekomtz1 <enekomartinezm@gmail.com>
2024-05-23 16:38:09 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/components/Cards.jsx
+++ 04-ExpenseTrackerApp/frontend/src/components/Cards.jsx
@@ -2,6 +2,7 @@
 - This code defines a React component named Cards.
 - It uses Apollo Client's useQuery hook to fetch data from a GraphQL API.
 - It retrieves transactions, authenticated user information, and user-specific transactions.
+- The component renders a list of transaction cards if data is successfully fetched.
 
 
 */
