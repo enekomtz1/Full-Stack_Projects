733c7f9c9021c82b23d3045ac24503b77cad751b
enekomtz1 <enekomartinezm@gmail.com>
2024-05-24 15:31:06 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/components/InputField.jsx
+++ 04-ExpenseTrackerApp/frontend/src/components/InputField.jsx
@@ -2,5 +2,6 @@
 - This code defines a functional React component called InputField.
 - It renders a labeled input field that can be customized through props.
 - The component accepts props for label, id, name, type, onChange event handler, and value.
+- It applies specific styling to the label and input field for better UI consistency.
 
 */
