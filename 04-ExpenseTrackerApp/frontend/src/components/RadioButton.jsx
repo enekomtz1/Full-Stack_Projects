878e9cfa22829b8558178155f58f59bddebb17d7
enekomtz1 <enekomartinezm@gmail.com>
2024-05-25 09:42:36 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/components/RadioButton.jsx
+++ 04-ExpenseTrackerApp/frontend/src/components/RadioButton.jsx
@@ -1,5 +1,6 @@
 /*
 - This code defines a functional React component named `RadioButton`.
 - It renders a styled radio button with an associated label.
+- The component receives several props to handle its state and appearance.
 
 */
