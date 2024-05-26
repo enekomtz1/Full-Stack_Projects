7c55677552612384dabe7bd7216694719b308649
enekomtz1 <enekomartinezm@gmail.com>
2024-05-26 10:19:29 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/components/TransactionForm.jsx
+++ 04-ExpenseTrackerApp/frontend/src/components/TransactionForm.jsx
@@ -2,5 +2,6 @@
 - This code defines a React functional component for a transaction form.
 - It uses Apollo Client's useMutation hook to handle GraphQL mutations.
 - The form collects transaction details such as description, payment type, category, amount, location, and date.
+- Upon submission, it sends the transaction data to the server and displays success or error messages using react-hot-toast.
 
 */
