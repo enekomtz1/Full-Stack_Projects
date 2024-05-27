0e9c1257ccf2919729877d7f097c1851c40ca8f7
enekomtz1 <enekomartinezm@gmail.com>
2024-05-27 19:19:00 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/graphql/mutations/transcation.mutation.js
+++ 04-ExpenseTrackerApp/frontend/src/graphql/mutations/transcation.mutation.js
@@ -1,4 +1,5 @@
 /*
 - This code defines GraphQL mutations for creating, updating, and deleting transactions.
+- It uses the gql tag from the @apollo/client library to construct the GraphQL queries.
 
 */
