dce0c9dfaa1d3cafc770becf13e0377bef3a6ca8
enekomtz1 <enekomartinezm@gmail.com>
2024-05-28 18:49:13 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/graphql/mutations/user.mutation.js
+++ 04-ExpenseTrackerApp/frontend/src/graphql/mutations/user.mutation.js
@@ -2,5 +2,39 @@
 - This code defines GraphQL mutations for user authentication.
 - The SIGN_UP mutation allows new users to sign up by providing necessary details.
 - The LOGIN mutation allows existing users to log in with their credentials.
-
+- The LOGOUT mutation logs out the currently authenticated user.
+- Each mutation returns specific fields upon successful execution.
 */
+
+import { gql } from "@apollo/client";
+
+// Define the SIGN_UP mutation for user registration
+export const SIGN_UP = gql`
+    mutation SignUp($input: SignUpInput!) {
+        signUp(input: $input) {
+            _id       // Return the user's unique ID
+            name      // Return the user's name
+            username  // Return the user's username
+        }
+    }
+`;
+
+// Define the LOGIN mutation for user authentication
+export const LOGIN = gql`
+    mutation Login($input: LoginInput!) {
+        login(input: $input) {
+            _id       // Return the user's unique ID
+            name      // Return the user's name
+            username  // Return the user's username
+        }
+    }
+`;
+
+// Define the LOGOUT mutation for logging out the current user
+export const LOGOUT = gql`
+    mutation Logout {
+        logout {
+            message   // Return a logout confirmation message
+        }
+    }
+`;
