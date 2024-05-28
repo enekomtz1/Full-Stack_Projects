83cc1f0de124b3a03be2aaeece8bbf8c021ed157
enekomtz1 <enekomartinezm@gmail.com>
2024-05-28 18:49:07 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/graphql/mutations/user.mutation.js
+++ 04-ExpenseTrackerApp/frontend/src/graphql/mutations/user.mutation.js
@@ -1,5 +1,6 @@
 /*
 - This code defines GraphQL mutations for user authentication.
 - The SIGN_UP mutation allows new users to sign up by providing necessary details.
+- The LOGIN mutation allows existing users to log in with their credentials.
 
 */
