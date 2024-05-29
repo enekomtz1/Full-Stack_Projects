15a1d30265b506d700fe2deb06bf86f08bcff40a
enekomtz1 <enekomartinezm@gmail.com>
2024-05-29 19:12:18 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/graphql/queries/user.query.js
+++ 04-ExpenseTrackerApp/frontend/src/graphql/queries/user.query.js
@@ -1,5 +1,6 @@
 /*
 - This code defines GraphQL queries using the Apollo Client library.
 - The first query, GET_AUTHENTICATED_USER, retrieves data about the currently authenticated user.
+- The second query, GET_USER_AND_TRANSACTIONS, retrieves data about a specific user and their transactions.
 
 */
