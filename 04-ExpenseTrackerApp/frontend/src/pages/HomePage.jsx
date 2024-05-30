40107565ebfd1d2244a46a027d99746cea6ec910
enekomtz1 <enekomartinezm@gmail.com>
2024-05-30 18:49:10 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/pages/HomePage.jsx
+++ 04-ExpenseTrackerApp/frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 /*
 - This code defines a React functional component for the home page of a financial tracking application.
+- It uses the Apollo Client to query transaction statistics and authenticated user data from a GraphQL API.
 
 */
