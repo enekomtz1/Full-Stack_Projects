4fc10c217528e3e52b881b88020cd11778913823
enekomtz1 <enekomartinezm@gmail.com>
2024-06-01 20:14:36 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/pages/NotFoundPage.jsx
+++ 04-ExpenseTrackerApp/frontend/src/pages/NotFoundPage.jsx
@@ -1,5 +1,6 @@
 /*
 - This code defines a React functional component called NotFound.
 - It displays a 404 error page indicating that the requested resource could not be found.
+- The page includes an image, a message, and a button to navigate back to the homepage.
 
 */
