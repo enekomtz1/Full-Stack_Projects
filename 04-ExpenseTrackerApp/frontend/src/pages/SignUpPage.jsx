4cfd3e2fca1c9aed5dd3a453305203ebee26b4d0
enekomtz1 <enekomartinezm@gmail.com>
2024-06-02 15:02:16 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/pages/SignUpPage.jsx
+++ 04-ExpenseTrackerApp/frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 /*
 - This code defines a Sign-Up page component for a React application.
 - It allows users to input their name, username, password, and gender to create an account.
+- The component uses Apollo Client to handle the sign-up mutation.
 
 */
