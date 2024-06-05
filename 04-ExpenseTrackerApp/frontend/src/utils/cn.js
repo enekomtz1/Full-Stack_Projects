5718ae355146997cac3f1dc1421bbc038e81e552
enekomtz1 <enekomartinezm@gmail.com>
2024-06-05 20:19:44 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/utils/cn.js
+++ 04-ExpenseTrackerApp/frontend/src/utils/cn.js
@@ -2,6 +2,7 @@
 - This code combines class names using `clsx` and merges them with Tailwind CSS classes using `twMerge`.
 - The `clsx` function is used to conditionally combine class names.
 - The `twMerge` function is used to merge Tailwind CSS classes, resolving any conflicts.
+- The `cn` function takes multiple input class names as arguments.
 
 */
 
