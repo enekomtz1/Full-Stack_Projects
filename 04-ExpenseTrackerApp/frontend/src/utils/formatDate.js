24e3b390d2cd9fda32bdc45538acca04283c3101
enekomtz1 <enekomartinezm@gmail.com>
2024-06-06 18:43:08 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/utils/formatDate.js
+++ 04-ExpenseTrackerApp/frontend/src/utils/formatDate.js
@@ -1,5 +1,6 @@
 /*
 - This code defines a function to format a timestamp into a readable date string.
 - The function parses the timestamp to ensure it's in milliseconds.
+- It then converts this timestamp into a Date object.
 
 */
