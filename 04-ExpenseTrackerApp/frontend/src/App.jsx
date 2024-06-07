b88076baacf6f154f64529472e5ffaf0bb71b028
enekomtz1 <enekomartinezm@gmail.com>
2024-06-07 19:48:41 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/App.jsx
+++ 04-ExpenseTrackerApp/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 /*
 - This code defines the main application component for a React application.
 - It sets up routing using React Router DOM.
+- It conditionally renders components based on user authentication.
 
 */
