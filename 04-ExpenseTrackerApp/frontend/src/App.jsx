14fa69c8298f48c92d71224c3f68ffc323a0cea1
enekomtz1 <enekomartinezm@gmail.com>
2024-06-07 19:48:57 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/App.jsx
+++ 04-ExpenseTrackerApp/frontend/src/App.jsx
@@ -2,5 +2,6 @@
 - This code defines the main application component for a React application.
 - It sets up routing using React Router DOM.
 - It conditionally renders components based on user authentication.
+- It uses Apollo Client to fetch the authenticated user data.
 
 */
