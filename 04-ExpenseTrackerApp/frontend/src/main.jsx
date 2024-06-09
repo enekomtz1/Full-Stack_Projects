24e2055603b11222e314bb492b1dea1d78dbc4df
enekomtz1 <enekomartinezm@gmail.com>
2024-06-09 21:12:05 +0200

    Adding code

--- 04-ExpenseTrackerApp/frontend/src/main.jsx
+++ 04-ExpenseTrackerApp/frontend/src/main.jsx
@@ -1,5 +1,6 @@
 /*
 - This code sets up a React application with routing, GraphQL, and a custom background.
 - It initializes the Apollo Client for GraphQL queries.
+- It configures the React Router for handling different routes.
 
 */
