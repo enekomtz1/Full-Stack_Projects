fea48fff952d635c16277a9e92da9cd34522ee94
enekomtz1 <enekomartinezm@gmail.com>
2024-06-14 19:29:30 +0200

    Adding code

--- 05-personal_portfolio/components/Hero.tsx
+++ 05-personal_portfolio/components/Hero.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
-	return <div className="pb-20 pt-36"></div>;
+	return (
+		<div className="pb-20 pt-36">
+			<div>
+				<Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" />
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
