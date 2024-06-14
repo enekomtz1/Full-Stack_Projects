bd2fdfe87b7c59559df775ae2283e80cafb465ba
enekomtz1 <enekomartinezm@gmail.com>
2024-06-14 19:30:38 +0200

    Adding code

--- 05-personal_portfolio/components/Hero.tsx
+++ 05-personal_portfolio/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
 	return (
 		<div className="pb-20 pt-36">
 			<div>
-				<Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" />
+				<Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
 			</div>
 		</div>
 	);
