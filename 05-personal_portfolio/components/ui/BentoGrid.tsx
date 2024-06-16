09363d48e187cf41ab88c0389f72cee35b261db1
enekomtz1 <enekomartinezm@gmail.com>
2024-06-16 20:21:39 +0200

    Adding code

--- 05-personal_portfolio/components/ui/BentoGrid.tsx
+++ 05-personal_portfolio/components/ui/BentoGrid.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "@/components/ui/GradientBG";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({ className, children }: { className?: string; children?: React.ReactNode }) => {
 	return <div className={cn("grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ", className)}>{children}</div>;
@@ -26,7 +28,7 @@ export const BentoGridItem = ({
 	return (
 		<div
 			className={cn(
-				"row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+				"row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
 				className
 			)}
 			style={{
@@ -38,11 +40,22 @@ export const BentoGridItem = ({
 				<div className="w-full h-full absolute">{img && <img src={img} alt={img} className={cn(imgClassName, "object-cover, object-center")} />}</div>
 			</div>
 
-			<div className={`absolute, right-0 -botom-5 ${id === 5 && "w-full opacity-80"}`}>{spareImg && <img src={spareImg} alt={spareImg} className="object-cover object-center" />}</div>
-			<div className="group-hover/bento:translate-x-2 transition duration-200">
-				<div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">{title}</div>
-				<div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">{description}</div>
+			<div className={`absolute, right-0 -botom-5 ${id === 5 && "w-full opacity-80"}`}>
+				{spareImg && <img src={spareImg} alt={spareImg} className="object-cover object-center w-full h-full" />}
 			</div>
+
+			{id === 6 && (
+				<BackgroundGradientAnimation>
+					<div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
+				</BackgroundGradientAnimation>
+			)}
+
+			<div className={cn(titleClassName, "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10")}>
+				<div className="font-sans font-extralight text-[#c1c2d3] text-xm md:text-xs lg:text-base z-10">{description}</div>
+				<div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">{title}</div>
+			</div>
+
+			{id === 3 && <GlobeDemo />}
 		</div>
 	);
 };
