75029ed11079ace986b9593d05be2272944e6cd7
enekomtz1 <enekomartinezm@gmail.com>
2024-06-20 00:10:27 +0200

    Adding code

--- 05-personal_portfolio/components/Clients.tsx
+++ 05-personal_portfolio/components/Clients.tsx
@@ -6,7 +6,7 @@ const Clients = () => {
 	return (
 		<div
 			className="py-20"
-			id="projects">
+			id="testimonials">
 			<h1 className="heading">
 				Kind words from{" "}
 				<span className="text-purple">satisfied clients</span>
