c7a87f090338c7a395f700e0749eeaca45f92211
enekomtz1 <enekomartinezm@gmail.com>
2024-06-23 20:30:53 +0200

    Adding code

--- 06-print_on_demand_project/src/app/configure/upload/Page.tsx
+++ 06-print_on_demand_project/src/app/configure/upload/Page.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 import React from "react";
 
 const Page = () => {
-	return <div className={cn('')}>page</div>;
+	return <div className={cn('relative h-full flex-1 my-16 w-full rounded-')}>page</div>;
 };
 
 export default Page;
