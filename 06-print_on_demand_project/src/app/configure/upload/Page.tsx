735c0960201a80e350de219a0b8483c70923d690
enekomtz1 <enekomartinezm@gmail.com>
2024-06-23 20:31:55 +0200

    Adding code

--- 06-print_on_demand_project/src/app/configure/upload/Page.tsx
+++ 06-print_on_demand_project/src/app/configure/upload/Page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
 	return (
 		<div
 			className={cn(
-				"relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 ",
+				"relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center",
 			)}>
 			page
 		</div>
