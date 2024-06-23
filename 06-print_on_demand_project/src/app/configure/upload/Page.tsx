efb68094ecc3cec43ac63b61adf79c4feb2d4d7f
enekomtz1 <enekomartinezm@gmail.com>
2024-06-23 20:30:06 +0200

    Adding code

---
+++ 06-print_on_demand_project/src/app/configure/upload/Page.tsx
@@ -0,0 +1,10 @@
+'use  client'
+
+import { cn } from "@/lib/utils";
+import React from "react";
+
+const Page = () => {
+	return <div className={cn('')}>page</div>;
+};
+
+export default Page;
--- 06-print_on_demand_project/src/components/Reviews.tsx
+++ 06-print_on_demand_project/src/components/Reviews.tsx
@@ -1,24 +1,28 @@
 "use client";
+
 import { HTMLAttributes, useEffect, useRef, useState } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { useInView } from "framer-motion";
 import { cn } from "@/lib/utils";
+import Phone from "./Phone";
+
 const PHONES = [
 	"/testimonials/1.jpg",
 	"/testimonials/2.jpg",
 	"/testimonials/3.jpg",
 	"/testimonials/4.jpg",
 	"/testimonials/5.jpg",
+	"/testimonials/6.jpg",
 ];
 
 function splitArray<T>(array: Array<T>, numParts: number) {
 	const result: Array<Array<T>> = [];
+
 	for (let i = 0; i < array.length; i++) {
 		const index = i % numParts;
 		if (!result[index]) {
 			result[index] = [];
 		}
-
 		result[index].push(array[i]);
 	}
 
@@ -31,7 +35,7 @@ function ReviewColumn({
 	reviewClassName,
 	msPerPixel = 0,
 }: {
-	reviews: string;
+	reviews: string[];
 	className?: string;
 	reviewClassName?: (reviewIndex: number) => string;
 	msPerPixel?: number;
@@ -46,7 +50,9 @@ function ReviewColumn({
 		const resizeObserver = new window.ResizeObserver(() => {
 			setColumnHeight(columnRef.current?.offsetHeight ?? 0);
 		});
+
 		resizeObserver.observe(columnRef.current);
+
 		return () => {
 			resizeObserver.disconnect();
 		};
@@ -56,13 +62,13 @@ function ReviewColumn({
 		<div
 			ref={columnRef}
 			className={cn("animate-marquee space-y-8 py-4", className)}
-			style={
-				{
-					"--marquee-duration": duration,
-				} as React.CSSProperties
-			}>
+			style={{ "--marquee-duration": duration } as React.CSSProperties}>
 			{reviews.concat(reviews).map((imgSrc, reviewIndex) => (
-				<Review />
+				<Review
+					key={reviewIndex}
+					className={reviewClassName?.(reviewIndex % reviews.length)}
+					imgSrc={imgSrc}
+				/>
 			))}
 		</div>
 	);
@@ -72,14 +78,37 @@ interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
 	imgSrc: string;
 }
 
-function Review({}: ReviewProps) {}
+function Review({ imgSrc, className, ...props }: ReviewProps) {
+	const POSSIBLE_ANIMATION_DELAYS = [
+		"0s",
+		"0.1s",
+		"0.2s",
+		"0.3s",
+		"0.4s",
+		"0.5s",
+	];
+
+	const animationDelay =
+		POSSIBLE_ANIMATION_DELAYS[
+			Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
+		];
+
+	return (
+		<div
+			className={cn(
+				"animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5",
+				className,
+			)}
+			style={{ animationDelay }}
+			{...props}>
+			<Phone imgSrc={imgSrc} />
+		</div>
+	);
+}
 
 function ReviewGrid() {
-	/* Check if the user is viewing the animation */
 	const containerRef = useRef<HTMLDivElement | null>(null);
 	const isInView = useInView(containerRef, { once: true, amount: 0.4 });
-
-	/* Split array */
 	const columns = splitArray(PHONES, 3);
 	const column1 = columns[0];
 	const column2 = columns[1];
@@ -88,12 +117,39 @@ function ReviewGrid() {
 	return (
 		<div
 			ref={containerRef}
-			className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-col-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3">
+			className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3">
 			{isInView ? (
 				<>
-					<ReviewColumn />
+					<ReviewColumn
+						reviews={[...column1, ...column3.flat(), ...column2]}
+						reviewClassName={(reviewIndex) =>
+							cn({
+								"md:hidden":
+									reviewIndex >=
+									column1.length + column3[0].length,
+								"lg:hidden": reviewIndex >= column1.length,
+							})
+						}
+						msPerPixel={10}
+					/>
+					<ReviewColumn
+						reviews={[...column2, ...column3[1]]}
+						className="hidden md:block"
+						reviewClassName={(reviewIndex) =>
+							reviewIndex >= column2.length ? "lg:hidden" : ""
+						}
+						msPerPixel={15}
+					/>
+					<ReviewColumn
+						reviews={column3.flat()}
+						className="hidden md:block"
+						msPerPixel={10}
+					/>
 				</>
 			) : null}
+			<div className="pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100" />
+
+			<div className="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100" />
 		</div>
 	);
 }
@@ -102,10 +158,9 @@ export function Reviews() {
 	return (
 		<MaxWidthWrapper className="relative max-w-5xl">
 			<img
-				className="absolute select-none hidden xl:block -left-32 top-1/3"
 				aria-hidden="true"
 				src="/what-people-are-buying.png"
-				alt="What People Are Buying"
+				className="absolute select-none hidden xl:block -left-32 top-1/3"
 			/>
 
 			<ReviewGrid />
